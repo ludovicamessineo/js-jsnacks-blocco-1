70d9a66e7d1ea4ee1b6ed73abb162ffe43c86a1a
ludovicamessineo <ludovica.messineo9@gmail.com>
2022-04-19 15:22:17 +0200

    third snack

---
+++ snack-3/js/script.js
@@ -0,0 +1,14 @@
+// jsnack3. Calcola la somma e la media dei primi 10 numeri.
+
+// Faccio la somma dei numeri da 1 a 10 con ciclo for
+let sum = 0
+for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
+}
+
+console.log(sum);
+
+// Calcolo la media
+
+const media = sum / 10;
+console.log(media);
